436e6b2b36e15a2e52859ba58311e8fd01c69ec5
Dmitry G. Anderson <dandgerson@gmail.com>
2019-06-29 22:28:13 +0300

    init project

---
+++ src/components/TodoList.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Todo from "./Todo";
+
+const TodoList = ({ todos }) => (
+  <ul className="todo-list">
+    {todos && todos.length
+      ? todos.map((todo, index) => {
+          return <Todo key={`todo-${todo.id}`} todo={todo} />;
+        })
+      : "No todos, yay!"}
+  </ul>
+);
+
+export default TodoList;
